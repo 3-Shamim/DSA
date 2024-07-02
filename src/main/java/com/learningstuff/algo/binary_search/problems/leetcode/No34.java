0c501f2d80882634d89197e71cf478273d57b6f9
Md Shamim <mdshamim723@gmail.com>
2024-07-02 09:16:37 +0600

    need to be completed

---
+++ src/main/java/com/learningstuff/algo/binary_search/problems/leetcode/No34.java
@@ -0,0 +1,35 @@
+package com.learningstuff.algo.binary_search.problems.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Md. Shamim
+ * Date: 1/7/24
+ * Time: 10:44 PM
+ * Email: mdshamim723@gmail.com
+ */
+
+
+/*
+ * LeetCode: https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/
+ */
+
+public class No34 {
+
+    public static int[] searchRange(int[] nums, int target) {
+
+        int[] result = new int[2];
+
+
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(Arrays.toString(searchRange(new int[]{5, 7, 7, 8, 8, 10}, 8)));
+
+    }
+
+}
