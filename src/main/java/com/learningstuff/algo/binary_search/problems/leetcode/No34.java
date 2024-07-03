7397cb8fced0d256a6ff8e22b3f770480c1e2433
Md Shamim <mdshamim723@gmail.com>
2024-07-03 08:14:35 +0600

    sovled leetcode 34

--- src/main/java/com/learningstuff/algo/binary_search/problems/leetcode/No34.java
+++ src/main/java/com/learningstuff/algo/binary_search/problems/leetcode/No34.java
@@ -10,7 +10,6 @@ import java.util.Arrays;
  * Email: mdshamim723@gmail.com
  */
 
-
 /*
  * LeetCode: https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/
  */
@@ -19,11 +18,44 @@ public class No34 {
 
     public static int[] searchRange(int[] nums, int target) {
 
-        int[] result = new int[2];
+        int[] result = {-1, -1};
 
+        if (nums == null || nums.length == 0) {
+            return result;
+        }
 
+        result[0] = find(nums, target, true);
+        result[1] = find(nums, target, false);
 
         return result;
+    }
+
+    private static int find(int[] nums, int target, boolean first) {
+
+        int start = 0, end = nums.length - 1;
+
+        int found = -1;
+
+        while (start <= end) {
+
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] == target) {
+                found = mid;
+                if (first) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else if (target > nums[mid]) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+
+        }
+
+        return found;
     }
 
     public static void main(String[] args) {
