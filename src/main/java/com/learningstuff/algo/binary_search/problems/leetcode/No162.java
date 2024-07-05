159c6fbbfac74374a280b1e95860e9cf35bfa961
Md Shamim <mdshamim723@gmail.com>
2024-07-05 07:24:29 +0600

    fixed

--- src/main/java/com/learningstuff/algo/binary_search/problems/leetcode/No162.java
+++ src/main/java/com/learningstuff/algo/binary_search/problems/leetcode/No162.java
@@ -29,28 +29,6 @@ public class No162 {
     }
 
     // Solve with binary search
-    public static int findPeakElement2(int[] arr) {
-
-        int start = 0, end = arr.length - 1;
-
-        while (start < end) {
-
-            int mid = start + (end - start) / 2;
-
-            if (mid != end && arr[mid] > arr[mid + 1]) {
-                end = mid;
-            } else if (mid != start && arr[mid] > arr[mid - 1]) {
-                start = mid;
-            } else {
-                start = mid + 1;
-            }
-
-        }
-
-        return start;
-    }
-
-    // Solve with binary search
     public static int findPeakElement(int[] arr) {
 
         int start = 0, end = arr.length - 1;
