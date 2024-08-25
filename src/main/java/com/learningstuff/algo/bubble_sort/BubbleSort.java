7f23822dbed5312adf9ac93a88883b54273b968b
Shamim <shamim.molla@vivasoftltd.com>
2024-08-25 16:26:41 +0600

    bubble sort added

---
+++ src/main/java/com/learningstuff/algo/bubble_sort/BubbleSort.java
@@ -0,0 +1,48 @@
+package com.learningstuff.algo.bubble_sort;
+
+import java.util.Arrays;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Md. Shamim Molla
+ * Email: shamim.molla@vivasoftltd.com
+ */
+
+public class BubbleSort {
+
+    private static int[] bubbleSort(int[] arr) {
+
+        for (int i = 0; i < arr.length - 1; i++) {
+
+            boolean swapped = false;
+
+            for (int j = 0; j < arr.length - i - 1; j++) {
+
+                if (arr[j] > arr[j + 1]) {
+
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+
+                    swapped = true;
+
+                }
+
+            }
+
+            if (!swapped) {
+                break;
+            }
+
+        }
+
+        return arr;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(Arrays.toString(bubbleSort(new int[]{4, 6, 3, 2, 1})));
+
+    }
+
+}
