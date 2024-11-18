17c8d0225d2c48e8516af7896c09a6287a7f9d61
Shamim <shamim.molla@vivasoftltd.com>
2024-11-18 17:56:23 +0600

    need to finalize

--- src/main/java/com/learningstuff/backtracking/NQueens.java
+++ src/main/java/com/learningstuff/backtracking/NQueens.java
@@ -1,5 +1,7 @@
 package com.learningstuff.backtracking;
 
+import java.util.Arrays;
+
 /**
  * Created by IntelliJ IDEA.
  * User: Md. Shamim
@@ -9,19 +11,68 @@ package com.learningstuff.backtracking;
 
 public class NQueens {
 
-    private static void nQueens(int n) {
+    private static void nQueens(boolean[][] board, int row) {
 
-        for (int i = 0; i < n; i++) {
+        if (row >= board.length) {
 
-//            if ()
+            for (boolean[] bR : board) {
+                System.out.println(Arrays.toString(bR));
+            }
+            System.out.println("=====================");
+
+            return;
+        }
+
+        for (int col = 0; col < board.length; col++) {
+
+            if (isQueenSafe(board, row, col)) {
+                board[row][col] = true;
+                nQueens(board, row + 1);
+                board[row][col] = false;
+            }
 
         }
 
     }
 
+    private static boolean isQueenSafe(boolean[][] board, int row, int col) {
+
+        for (int i = 0; i < row; i++) {
+
+            if (board[i][col]) {
+                return false;
+            }
+
+        }
+
+        for (int i = 0; i < Math.min(row, col); i++) {
+
+            if (board[row - (i + 1)][col - (i + 1)]) {
+                return false;
+            }
+
+        }
+
+        for (int i = Math.max(row, col) + 1; i < board.length; i++) {
+
+            int
+
+            if (board[board.length - i][]) {
+                return false;
+            }
+
+        }
+
+        return true;
+    }
+
     public static void main(String[] args) {
 
-        nQueens(4);
+        int n = 4;
+
+        boolean[][] board = new boolean[n][n];
+
+        nQueens(board, 0);
 
     }
 
