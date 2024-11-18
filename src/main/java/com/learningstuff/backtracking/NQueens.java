68e676978b4a8cc3cdbb5cd14c8c5e8a4eeb2413
Shamim <mdshamim723@gmail.com>
2024-11-18 09:27:55 +0600

    need to be complete

---
+++ src/main/java/com/learningstuff/backtracking/NQueens.java
@@ -0,0 +1,28 @@
+package com.learningstuff.backtracking;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Md. Shamim
+ * Date: 11/17/24
+ * Email: mdshamim723@gmail.com
+ */
+
+public class NQueens {
+
+    private static void nQueens(int n) {
+
+        for (int i = 0; i < n; i++) {
+
+//            if ()
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        nQueens(4);
+
+    }
+
+}
