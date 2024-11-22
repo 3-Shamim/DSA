fa0fb7126f0e9d1c955f6e07d2f592404e70ea0c
Shamim <shamim.molla@vivasoftltd.com>
2024-11-22 12:47:11 +0600

    sudoku solve

--- src/main/java/com/learningstuff/backtracking/leetcode/No37.java
+++ src/main/java/com/learningstuff/backtracking/leetcode/No37.java
@@ -35,6 +35,7 @@ public class No37 {
         boolean completed = true;
 
         for (int i = 0; i < board.length; i++) {
+
             for (int j = 0; j < board[0].length; j++) {
 
                 row = i;
