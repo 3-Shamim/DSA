2397564a48ada7a2cefbe4ea897d1ab30e1cf159
Shamim <mdshamim723@gmail.com>
2025-01-10 23:30:59 +0600

    lps - longest suffix prefix table calculate, now kmn algo need to be implemented

--- src/main/java/com/learningstuff/algo/string_pattern_match/KMP.java
+++ src/main/java/com/learningstuff/algo/string_pattern_match/KMP.java
@@ -1,5 +1,7 @@
 package com.learningstuff.algo.string_pattern_match;
 
+import java.util.Arrays;
+
 /**
  * Created by IntelliJ IDEA.
  * User: Md. Shamim
@@ -11,8 +13,42 @@ public class KMP {
 
     public static void main(String[] args) {
 
+        String text = "abxabcabcaby";
+        String pattern = "abcaby";
 
+        System.out.println(kmpSolution(text, pattern));
 
+    }
+
+    private static boolean kmpSolution(String text, String pattern) {
+
+        int[] lps = createLPS(pattern);
+        System.out.println(Arrays.toString(lps));
+
+        return false;
+    }
+
+    private static int[] createLPS(String pattern) {
+
+        int[] ans = new int[pattern.length()];
+
+        ans[0] = 0;
+
+        int matchIndex = 0;
+
+        for (int i = 1; i < pattern.length(); i++) {
+
+            if (pattern.charAt(matchIndex) == pattern.charAt(i)) {
+                matchIndex++;
+                ans[i] = matchIndex;
+            } else {
+                matchIndex = 0;
+                ans[i] = matchIndex;
+            }
+
+        }
+
+        return ans;
     }
 
 }
