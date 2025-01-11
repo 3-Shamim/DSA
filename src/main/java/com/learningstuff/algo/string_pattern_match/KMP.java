d769a43e3952a76f51fa9426a20c001ff194e4aa
Shamim <mdshamim723@gmail.com>
2025-01-11 19:33:28 +0600

    kmp done

--- src/main/java/com/learningstuff/algo/string_pattern_match/KMP.java
+++ src/main/java/com/learningstuff/algo/string_pattern_match/KMP.java
@@ -1,7 +1,5 @@
 package com.learningstuff.algo.string_pattern_match;
 
-import java.util.Arrays;
-
 /**
  * Created by IntelliJ IDEA.
  * User: Md. Shamim
@@ -13,8 +11,8 @@ public class KMP {
 
     public static void main(String[] args) {
 
-        String text = "abxabcabcaby";
         String pattern = "abcaby";
+        String text = "abxabcabcaby";
 
         System.out.println(kmpSolution(text, pattern));
 
@@ -23,32 +21,60 @@ public class KMP {
     private static boolean kmpSolution(String text, String pattern) {
 
         int[] lps = createLPS(pattern);
-        System.out.println(Arrays.toString(lps));
 
-        return false;
-    }
+        int tLen = text.length();
+        int pLen = pattern.length();
 
-    private static int[] createLPS(String pattern) {
+        int tIndex = 0;
+        int pIndex = 0;
 
-        int[] ans = new int[pattern.length()];
+        while (tIndex < tLen && pIndex < pLen) {
 
-        ans[0] = 0;
-
-        int matchIndex = 0;
-
-        for (int i = 1; i < pattern.length(); i++) {
-
-            if (pattern.charAt(matchIndex) == pattern.charAt(i)) {
-                matchIndex++;
-                ans[i] = matchIndex;
+            if (text.charAt(tIndex) == pattern.charAt(pIndex)) {
+                tIndex++;
+                pIndex++;
             } else {
-                matchIndex = 0;
-                ans[i] = matchIndex;
+                if (pIndex == 0) {
+                    tIndex++;
+                } else {
+                    pIndex = lps[pIndex - 1];
+                }
             }
 
         }
 
-        return ans;
+        return pIndex == pLen;
+    }
+
+    // Test Data
+    // ababcababa = 0012012343
+    // abcabcda = 00012301
+    // abab = 0012
+    private static int[] createLPS(String pattern) {
+
+        int[] lps = new int[pattern.length()];
+
+        lps[0] = 0;
+
+        int len = 0;
+
+        for (int i = 1; i < pattern.length(); i++) {
+
+            if (pattern.charAt(i) == pattern.charAt(len)) {
+                len++;
+                lps[i] = len;
+            } else {
+                if (len == 0) {
+                    lps[i] = 0;
+                } else {
+                    i--;
+                    len = lps[len - 1];
+                }
+            }
+
+        }
+
+        return lps;
     }
 
 }
